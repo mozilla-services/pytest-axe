cd90bccab8a7f1518aed8ba0eb192d6b0579df55
Kimberly <ksereduck@mozilla.com>
2018-03-27 11:16:25 -0600

    adding functionalities removed from axe-selenium-python

--- pytest_axe/pytest_axe.py
+++ pytest_axe/pytest_axe.py
@@ -28,3 +28,57 @@ def pytest_collection_modifyitems(config, items):
     for item in items:
         if "accessibility" in item.keywords:
             item.add_marker(skip_a11y)
+
+class PytestAxe(Axe):
+
+    def __init__(self, selenium):
+        super(PytestAxe, self).__init__(selenium)
+
+    def get_rules(self):
+        """Return array of accessibility rules."""
+        response = self.selenium.execute_script('return axe.getRules();')
+        return response
+
+    def run(self, context=None, options=None, impact=None):
+        """
+        Inject aXe, run against current page, and return rules & violations.
+        """
+        self.inject()
+        data = self.execute(context, options)
+        violations = dict((rule['id'], rule) for rule in data['violations'] if self.impact_included(rule, impact))
+
+        return violations
+
+    def impact_included(self, rule, impact):
+        """
+        Function to filter for violations with specified impact level, and all
+        violations with a higher impact level.
+        """
+        if impact == 'minor' or impact is None:
+            return True
+        elif impact == 'serious':
+            if rule['impact'] != 'minor':
+                return True
+        elif impact == 'critical':
+            if rule['impact'] == 'critical':
+                return True
+        else:
+            return False
+
+    def analyze(self, context=None, options=None, impact=None):
+        """Run aXe accessibility checks, and write results to file."""
+        disabled = environ.get('ACCESSIBILITY_DISABLED')
+        if not disabled or disabled is None:
+            violations = self.run(context, options, impact)
+
+            # Format file name based on page title and current datetime.
+            t = time.strftime("%m_%d_%Y_%H:%M:%S")
+            title = self.selenium.title
+            title = re.sub('[\s\W]', '-', title)
+            title = re.sub('(-|_)+', '-', title)
+
+            # Output results only if reporting is enabled.
+            if environ.get('ACCESSIBILITY_REPORTING') == 'true':
+                # Write JSON results to file if recording enabled
+                self.write_results('results/%s_%s.json' % (title, t), violations)
+            assert len(violations) == 0, self.report(violations)
