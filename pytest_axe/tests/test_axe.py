c882a8991935cdaef2613dafde2f65b7b60646e0
Kimberly <ksereduck@mozilla.com>
2018-10-03 13:30:50 -0600

    fixing file path

--- pytest_axe/tests/test_axe.py
+++ pytest_axe/tests/test_axe.py
@@ -2,10 +2,12 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 import json
+import os
 
 from ..parameterize_tests import *  # NOQA
 
-filename = "results.json"
+filename = os.path.join(os.path.dirname(__file__), "results.json")
+
 if filename:
     with open(filename, "r") as f:
         test_data = json.load(f)
